9900f9bdbcb0a88e9553d55e78044a99b8fd8ded
SergeyL <ken4o@mail.ru>
2019-12-05 15:02:37 +0200

    added simple header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Head = styled.header`
+  display: flex;
+  background-color: black;
+  color: white;
+  flex-direction: row;
+  justify-content: c;
+`;
+
+const Header = () => <Head>haha</Head>;
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Fetch from "./components/fetch.js";
-
-ReactDOM.render(<Fetch />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Fetch from './components/fetch.js';
+import Header from './components/header';
+ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<Fetch />, document.getElementById('root'));
