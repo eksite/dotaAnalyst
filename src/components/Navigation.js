af7feba6469468e0820d709b63277f572436998b
SergeyL <ken4o@mail.ru>
2020-01-23 11:47:47 +0200

    delete useless veriable in navigator

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-const Links = ({ items }) => {
-    const res = Object.entries(items).map(arr => (
+const Links = ({ items }) =>
+    Object.entries(items).map(arr => (
         <Link key={arr[1]} to={arr[1]}>
             {arr[0]}
         </Link>
     ));
-    return res;
-};
 export default Links;
