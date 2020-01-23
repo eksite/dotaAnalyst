b22af8bc6476c44ea2b4897e43c1357e434580a6
SergeyL <ken4o@mail.ru>
2020-01-23 11:57:40 +0200

    delete useless information

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import Navigation from './Navigation';
-=======
-// import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import './header.css';
->>>>>>> 62f416dfca5468616eff2e3c894105dd0bf3b8c9
 
 // const Head = styled.header`
-//   display: flex;
-//   background-color: black;
+//   display: fle
+    //   background-color: black;
 //   color: white;
 //   flex-direction: row;
 //   justify-content: c;
